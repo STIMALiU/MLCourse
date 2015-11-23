77fd12f5753cff466c9bc9c200137b0c141c8d74
Mattias Villani <mattias.villani@gmail.com>
2015-11-23 15:11:41 +0100

    Fixed typo regarding how sigmaF entered the kernel function.

--- GPandMixtures/Code/GaussianProcesses.R
+++ GPandMixtures/Code/GaussianProcesses.R
@@ -12,7 +12,7 @@ SquaredExpKernel <- function(x1,x2,sigmaF=1,l=3){
   n2 <- length(x2)
   K <- matrix(NA,n1,n2)
   for (i in 1:n2){
-    K[,i] <- sigmaF*exp(-0.5*( (x1-x2[i])/l)^2 )
+    K[,i] <- sigmaF^2*exp(-0.5*( (x1-x2[i])/l)^2 )
   }
   return(K)
 }
